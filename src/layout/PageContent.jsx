7e6da967185e01f97769455bb68583f1c396369d
ozge <karabayozge@gmail.com>
2024-10-22 11:54:12 +0300

    T2: Home Page done template #2

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,4 +1,5 @@
 import CarouselHome from "@/components/custom/Carousel";
+import FeaturedPost from "@/components/custom/FeaturedPost";
 import PickCard from "@/components/custom/PickCard";
 import ProductCard from "@/components/custom/ProductCard";
 import { Button } from "@/components/ui/button";
@@ -10,57 +11,79 @@ const PageContent = () => {
     <>
       <div>
         <CarouselHome />
-        <div className="flex flex-col items-center h-2/3">
+        <div className="flex flex-col items-center text-center h-2/3">
           <h2>EDITOR’S PICK </h2>
           <p>Problems trying to resolve the conflict between </p>
-          <div className="grid grid-cols-1 grid-rows-6 md:grid-cols-4 md:grid-rows-2 gap-4 w-3/4">
-            <div className="col-span-1 row-span-2 md:col-span-2 md:row-span-2 h-96 ">
+          <div className="grid grid-cols-1 gap-4 w-3/4 md:grid-cols-4 md:grid-rows-2">
+            <div className="h-64 md:h-96 md:col-span-2 md:row-span-2">
               <PickCard />
             </div>
 
-            <div className="col-span-1 row-span-2 md:col-span-1 md:row-span-2">
+            <div className="h-64 md:h-96 md:col-span-1 md:row-span-2">
               <PickCard />
             </div>
 
-            <div className="col-span-1 row-span-1 md:col-span-1 md:row-span-1  ">
+            <div className="h-64 md:h-48 md:col-span-1 md:row-span-1">
               <PickCard />
             </div>
 
-            <div className="col-span-1 row-span-1 md:col-span-1 md:row-span-1 ">
+            <div className="h-64 md:h-48 md:col-span-1 md:row-span-1">
               <PickCard />
             </div>
           </div>
         </div>
-        <div className=" w-full flex items-center justify-center flex-col">
+        <div className=" w-full flex items-center justify-center text-center flex-col">
           <h3>Featured Products</h3>
           <h2>BESTSELLER PRODUCTS </h2>
           <p>Problems trying to resolve the conflict between </p>
           <div className="flex flex-col w-3/4 items-center justify-between lg:flex-row lg:flex-wrap lg:gap-x-4 lg:gap-y-8">
             {Array.from({ length: itemCount }).map((_, index) => (
-              <div className="w-1/5">
+              <div className="w-full lg:w-1/5">
                 <ProductCard />
               </div>
             ))}
           </div>
         </div>
         <CarouselHome />
-        <div className="w-full flex">
+        <div className="w-full flex flex-col-reverse text-center md:flex-row">
           <div className="lg:w-1/3">
             <img
               src="https://picsum.photos/400/300"
               className="object-cover w-full"
             />
           </div>
-          <div className="lg:w-2/3">
-            <h3>Summer 2020</h3>
-            <h2>BESTSELLER PRODUCTS </h2>
-            <p>
-              We know how large objects will act, but things on a small scale.{" "}
-            </p>
-            <Button>
-              Buy Now
-            </Button>
-            
+          <div className="md:w-2/3 md:flex md:flex-col md:justify-center md:items-center">
+            <div className="md:p-12">
+              <h3>Summer 2020</h3>
+              <h2>Part of the Neural Universe </h2>
+              <p>
+                We know how large objects will act, but things on a small scale.
+              </p>
+              <div className="w-full flex gap-4 flex-col justify-center items-center md:flex-row ">
+                <Button className="w-1/3 md:w-1/5">Buy Now</Button>
+                <Button variant="outline" className="w-1/3 md:w-1/5">
+                  Read More
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:flex md:flex-col md:items-center">
+        <h3 className="text-secondary text-center">Practice Advice</h3>
+              <h2 className=" text-center">Featured Products </h2>
+              <p className=" text-center">  
+              Problems trying to resolve the conflict between the two major 
+              </p>
+              <div className="md:flex md:w-2/3 "> 
+          <div className="md:w-1/3">
+          <FeaturedPost/>
+          </div>
+          <div className="md:w-1/3">
+          <FeaturedPost/>
+          </div>
+          <div className=" md:w-1/3">
+          <FeaturedPost/>
+          </div>
           </div>
         </div>
       </div>
