42ed20e951ba313cf7423f81afa3e1a8621b3f58
ozge <karabayozge@gmail.com>
2024-11-05 14:37:41 +0300

    ADD TODO

--- src/layout/PageContentShop.jsx
+++ src/layout/PageContentShop.jsx
@@ -24,6 +24,7 @@ import { setFilter, setOffset } from "@/redux/actions/productActions";
 import { Input } from "@/components/ui/input";
 
 export default function PageContentShop() {
+  {/* TODO /shop location fix */}
   const dispatch = useDispatch();
   const history = useHistory();
   const { gender, category, categoryId } = useParams();
