10f011d756e9a673030a704df07119dd17f6ed86
ozge <karabayozge@gmail.com>
2024-11-06 13:02:41 +0300

    add dropdown template design

--- src/components/custom/ShoppingCartDropdown.jsx
+++ src/components/custom/ShoppingCartDropdown.jsx
@@ -8,23 +8,31 @@ const ShoppingCartDropdown = ({ isOpen, onClose }) => {
   return (
     <div className={`absolute right-0 mt-2 w-64 bg-white shadow-lg ${isOpen ? 'block' : 'hidden'} z-50`}>
       <div className="p-4">
-        <h2 className="font-bold text-lg">Shopping Cart</h2>
+        <h2 className="font-bold text-lg">Sepetim({cart.length} Ürün)</h2>
         {cart.length === 0 ? (
-          <p>Your cart is empty.</p>
+          <p>Sepetiniz Boş</p>
         ) : (
           cart.map((item) => (
             <div key={item.product.id} className="flex justify-between items-center py-2">
-              <div>
+              <div className='flex'>
+                <div className='w-1/3'> <img src={item.product.images[0].url}/></div>
+               <div>
                 <p>{item.product.name}</p>
-                <p>Count: {item.count}</p>
-                <p>Price: ${item.product.price.toFixed(2)}</p>
+                <p>Adet: {item.count}</p>
+                <h3 className='text-primary'>{`${item.product.price.toFixed(2)} TL`}</h3>
+                </div>
               </div>
             </div>
           ))
         )}
-        <Link to="/cart" className="block text-center mt-4 bg-primary text-white py-2 rounded">
-          Go to Cart
+        <div className='flex justify-between'>
+        <Link to="/cart" className="block text-center mt-4 text-sm bg-primary text-white rounded w-2/5">
+          Sepete Git
         </Link>
+        <Link to="/order" className="block text-center mt-4 text-sm bg-primary text-white  rounded w-1/2">
+          Siparişi Tamamla
+        </Link>
+        </div>
       </div>
     </div>
   );
